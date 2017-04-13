f643f15434168815cf1251fbf145756d26272117
asashin227 <asakura_s@AsakuranoiMac.local>
2017-04-13 09:27:32 +0900

    Fix for Xcode 8.3.

--- ASAKvcModel/ViewController.swift
+++ ASAKvcModel/ViewController.swift
@@ -9,17 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let dic: [String : Any] = ["str0" : "a", "str1" : "b", "str2" : "b", "num0" : 1, "num1" : 2, "num2" : 3, "errorKey" : "This key is error."] as [String : Any]
+        let dic = ["str0" : "a",
+                   "str1" : "b",
+                   "str2" : "c",
+                   "num0" : 1,
+                   "num1" : 2,
+                   "num2" : 4,
+                   "errorKey" : "This key is not working."] as [String : AnyObject]
         let obj: Object = Object(dictionary: dic)
         
         print("str0 : \(obj.str0)")
-        print("str1 : \(obj.str0)")
-        print("str2 : \(obj.str0)")
-        print("num0 : \(obj.num0)") 
+        print("str1 : \(obj.str1)")
+        print("str2 : \(obj.str2)")
+        print("num0 : \(obj.num0)")
         print("num1 : \(obj.num1)")
         print("num2 : \(obj.num2)")
         print("dictionaryObject : \(obj.dictionaryObject())")
